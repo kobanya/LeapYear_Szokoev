ec83ada2fd8cbf72730af268451b80602f8d4f5c
kobanya <nagy.bela.budapest@gmail.com>
2022-10-10 21:27:43 +0200

    javítás

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int year;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any Year / Irj be egy évszámot:");
+        year = scan.nextInt();
+        scan.close();
+        boolean isLeap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    isLeap = true;
+                else
+                    isLeap = false;
+            }
+            else
+                isLeap = true;
+        }
+        else {
+            isLeap = false;
+        }
+
+        if(isLeap==true)
+            System.out.println(year + " is a Leap Year - " + year +" szökőév!");
+        else
+            System.out.println(year + " is not a Leap Year - "+year + " nem szökőév");
     }
 }
